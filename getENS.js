02a1ae0492d5749ee196bd7c22f253f00488411d
aeroxi <magictalentx@gmail.com>
2022-06-12 16:53:17 +0800

    fix

--- getENS.js
+++ getENS.js
@@ -1,6 +1,11 @@
 import { ethers } from "ethers";
+import { kings } from "./king.js"
+
 const provider = new ethers.providers.WebSocketProvider(
     'wss://eth-mainnet.alchemyapi.io/v2/vUc8G6g7FAl8UrTYwC9o3pZTyDbZ1wgi'
   )
-let result = await provider.lookupAddress("0x5555763613a12D8F3e73be831DFf8598089d3dCa");
-console.log(result)
+
+for (const king of kings) {
+    console.log(await provider.lookupAddress(king.minter))
+}
+
---
+++ king.js
@@ -0,0 +1,467 @@
+export const kings = [
+    {
+        minter: '0xeea1e4702791b953e44b6977dcbcbff7a665839a',
+        goldenAmount: 11,
+        badAmount: 0
+      },
+      {
+        minter: '0x888c65c45923d565dee48b6e98ae5ed9b668d635',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0x20a06170ed456701d48370f3739cba119bac1455',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0x9639da3faefd13c8c8a5f3e7b400c57ccdd2ffa6',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0x980623beeae1795c2f7510b4da18db1a71cd5d76',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0xd2de94d56ae24491a3e58819eedad7a9287b0a94',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0xe831aaddd68bf03955423e8202553ed820b596fd',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0x0d1b84f177b1f9559ffa4fd92e56385b4a789923',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0xcff606f995f238a4dfcd47c8635694d2486038f2',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0xf100c3dae1839ff65b86024ac6866968ca527f2e',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0xdfb569285ddda3c42fabf64715c795f47fd9fe6d',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0xc3f696d9a6cf2d6d63c3de704a910c8946cbc9d2',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0x9c78aebb7b702fe06300b4ffd308b2dbb63a8888',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0x7546f827fc07c837e3cd920d25461a6e406eba92',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0xc2c1cbae9bee8c610b2c81045f893b00a08c7be5',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0x9708367387afaba8cf181679dbf64e273961cc3b',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0x7399b0f778ab2387d934884878ca03ec13072f2c',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0xb314fcf5ebb2bedbac19e039da67bfcc8fcf533f',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0x04164f33830277aff6a3d86c8b28ae775125e9e8',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0x4bfb241c0143e379147699724838e2b5a2e9c507',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0xc11ccfb7adafd0d457eedbeaa91d2c397affea4f',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0xabefac45d9dfb4c44adc1137f967284d763827d4',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0x41430dd31a3b3e8c3b44967b489c26965c100f9f',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0xb22da51b44ab4c24d17925849a1cb6aa2806d249',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0xfcb08c31a0b71e2e8eca7a011271e63e7f829677',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0xbe7bc3963c590407d3c0bbe38fa89b1089cf89d8',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0xc9f2ea810afd13993d90d54b6eafd71ce1721fb8',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0x26e359a059e0f8bab6b39ee2c1d58f899889f893',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0xc00c5976ba159282f8fed33ef09a83b6e4605670',
+        goldenAmount: 10,
+        badAmount: 0
+      },
+      {
+        minter: '0xc5a4e4b32dc4526e05b8919f9c84961fef666666',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0x8888887a5e2491fec904d90044e6cd6c69f1e71c',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0x052d253e84ad98257b699eeb30512e1687fd47a0',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0x61834529f1abf64546920a546cdd401daeb20b92',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0x8e6324c4f6b6cf58db09cbead65dd5d843df3fdd',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0x9d622ff9f7c910053f5e15afaa2c07e6ccfdb328',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0xda3741aec553df357ea6f2681d9621dc191e6577',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0xc9832c4a864e5802fe81fdf2ea35ba8038631fc8',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0xb7f70e01fc9b5ea2d99a90312109cadd6336f5c3',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0x80dfeec043359bec1b2527f994f6fddf0744f241',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0x0bcfdba1f641a447873ef079721d326c570816c1',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0x3ccc9cb5178a77556d5a419bfef2366ef85bf086',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0x6b183256dee5839bfed9f863accba2bc1a92425f',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0xb8a8d75e5cd023597366380ddf5c561972f35c58',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0x390ef97588d684cfd9ab123cbaef7b152a1b9c0e',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0x702aa2167e135d17861c7d2465e18db332547c21',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0x12c125e51cad03b3740272bd62713b76051fbf54',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0x9264ef74e0a2d76a8aba10bb2db507814921cc6e',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0x3c5f0b54723d750c2dc7a1041e7742cdb8888888',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0x99183da291d488ddddea91b34020a88fbbc9fe82',
+        goldenAmount: 9,
+        badAmount: 0
+      },
+      {
+        minter: '0x55447a219b092c89194206d905bbbdb27b666666',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xfc68b1f73e4a8d04195fdb445597b8afd76fbdd1',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x00000031fd4b1a11ee2749f47d66f561b45eaf31',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x99999983c70de9543cdc11db5de66a457d241e8b',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xffffff5800b709071d4adc74759ae4b89bef2a9d',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x3d6f2b7029473fedaf2a46ba38b198af6e999999',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xaaaa2555cd54573a0cf4ec3fbfd66f4d3b055555',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x555555b63d1c3a8c09fb109d2c80464685ee042b',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xda86021e5a5370006c4ae6ea74c470878f36a9a3',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x885da2b21ab4b6badddf57a24d26021344703b4e',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x57d37ea56e0a5fd0a2f3c21dfa7f9feaac9327f6',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x7d872f8ec8e6463d06e8f99f4f0e9dc5f2b15877',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xd7478258e8bf01f00a0d3576d42c707b26370dd7',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xb1aa880d6bc3162c823745aafbce219de5832aec',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x1a05a7e4cc57897fcad9acf24f8418699afb6469',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x907435900a848957a22c1f0ce9f12509b16bcbc2',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xbfa012f3960db1906433c6c817c42315c17f6671',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x492f6a409dd175a5475a98b226675ca2ee0af46d',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xc53af37b73818d09876db5da099b569be8b4379e',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x7777777c6fe3cdc47c7ffb2719fdbe46674f6ab9',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x80ff71445260219fc65707d32a20fba485cac0a3',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x5ee2ae1b43e138be7386d422ca955cfc4ace67aa',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xddbcb2cabf7f10372c41ee3b532f1bc4f4a96a74',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x6846041e1d80dd5af8f1e48d6272e621809f5f88',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xdf5e2c4807d9afd9e3fe87414eaed8b7d1d3f0c5',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x80b5cb6f3eac113b845f7103c8bda6afd59f1b01',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x9efcd63cf114521f1892d6ecb070266460111d04',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x1ef63a90a251411232e8fd1ad46144b9634dc12c',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xe6509df255c38b8d56b89da3cb0022fe18daf558',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xe002f3465e6943ecb865e984eb5388a5b960fcf5',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x4b2143be9ee6cc4ffb3f9746c853ad60194ed77b',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x00af15f23c28993887d5de957dc22657a376bb04',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x9be464c3c777bc74aec1ac6bb5adba8e4324870a',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xf2659a2b2b928a0555bf1596ebf2c30aa4b34a31',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x2d3f8910fa63054075561c30db3ce9297c232b5a',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xcd34e5cc0404b9fa69a8b72c4f14ffdc3872f51d',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x54adc7aef76b889e4f6c3b881bc88959727f6404',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xc12bd91a331079d5b6ccd1ffbb063c95fb861bae',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xa3d67d56a06ad6c253a57c6d4bdaf2948c08d270',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xd97fdaa7678104548f05e814235fee9870270165',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xfb39f5febf5264172f8dcc5e5499ea8651182eef',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x51d2ef424e833f4856fa9c311b480d274bb906cc',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0xea91c93e872c2ad6986bfc048312d07e0ae977f1',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+      {
+        minter: '0x01b637c3a105344de34d897a12134afb8926126a',
+        goldenAmount: 8,
+        badAmount: 0
+      },
+]
