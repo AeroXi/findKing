5dd65eef6b8d3204bda9e51c695b0c77fce6ee36
aeroxi <magictalentx@gmail.com>
2022-06-12 16:32:23 +0800

    add getENS

---
+++ getENS.js
@@ -0,0 +1,5 @@
+import { ethers } from "ethers";
+const provider = new ethers.providers.JsonRpcProvider('wss://eth-mainnet.alchemyapi.io/v2/vUc8G6g7FAl8UrTYwC9o3pZTyDbZ1wgi');
+
+let result = await provider.lookupAddress("0x5555763613a12D8F3e73be831DFf8598089d3dCa");
+console.log(result)
